74487c69126af700b8acc5330f193dd1aa16d4e6
Spiros Ioannou <sivann@mute.netmode.ece.ntua.gr>
2014-08-01 18:54:23 +0300

    using bootstrap css

--- php/listdir.php
+++ php/listdir.php
@@ -17,43 +17,62 @@ echo "<title>AudioPlayer :: ".basename($path)."</title>\n";
 if (isset($_GET['debug'])) print_r($dirinfo);
 ?>
 
-<div id='outer'> <!--outer -->
+<div class='container-fluid'> <!--outer -->
 
+	<div class='row'> 
+		<div class='col-md-3'>  <!-- left col -->
 
+			<div class='row'>
+				<div class="col-md-12 contentbox"> <!-- search -->
+					<div class='content_boxtitle'>
+						<span>Search</span>
+					</div>
 
-
-	<div id='container1'> 
-		<div id='search'  class='content_box'>
-		<div class='content_boxtitle'>
-			<span>Search</span>
-		</div>
-			<div class="input-group input-group-sm">
-				<input class="form-control" placeholder="Search" id='searchbox'>
+					<div class="input-group">
+					  <div class="input-group-btn">
+						<button type="button" class="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown">Action <span class="caret"></span></button>
+						<ul class="dropdown-menu" role="menu">
+						  <li><a href="#">Action</a></li>
+						  <li><a href="#">Another action</a></li>
+						  <li><a href="#">Something else here</a></li>
+						  <li class="divider"></li>
+						  <li><a href="#">Separated link</a></li>
+						</ul>
+					  </div><!-- /btn-group -->
+					  <input type="text" class="form-control input-sm">
+					</div><!-- /input-group -->
+				</div>
 			</div>
-		</div>
 
-		<div id='folder_images' class='content_box' >
-		<div class='content_boxtitle'>
-			<span>Artist Photos</span>
-		</div>
-		<?  printfolderimages(); ?>
-		</div>
+			<div class='row'>
+				<div id='folder_images' class='col-md-12 contentbox' > <!-- images -->
+					<div class='content_boxtitle'>
+						<span>Artist Photos</span>
+					</div>
+					<?  printfolderimages(); ?>
+				</div>
+			</div><!-- row -->
+
+			<div class='row'>
+				<div id='artistinfo'  class='col-md-12 contentbox'> <!-- artistinfo -->
+					<div class='content_boxtitle'>
+						<span>Artist Info</span>
+					</div>
+					<?
+					printArtistInfo();
+					?>
+				</div>
+			</div><!--row-->
 
 
-		<div id='artistinfo'  class='content_box'>
-		<div class='content_boxtitle'>
-			<span>Artist Info</span>
-		</div>
-		<?
-		printArtistInfo();
-		?>
-		</div>
-	</div><!-- container1 -->
 
-	<div id='main' >
-		<?php require 'main_div_inc.php'; ?>
-	</div><!-- main div -->
+		</div> <!-- left col-->
 
+		<div class='col-md-9'>
+			<?php require 'main_div_inc.php'; ?>
+		</div><!-- right col div -->
+
+	</div><!-- row -->
 
 </div> <!--outer -->
 
